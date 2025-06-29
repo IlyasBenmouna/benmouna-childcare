190539c81f6cbdd74d2c4058b4403f69d5e36e8b
ilyas.benmouna <ilyas.benmouna@ocado.com>
2025-06-29 21:08:18 +0100

    remove number

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -52,12 +52,6 @@ export const AboutMe = () => (
                     <div className="flex flex-col gap-3">
                         <p className="text-[#4e8597] text-base font-normal leading-normal flex items-center gap-3">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256">
-                                <path d="M231.88,175.08A56.26,56.26,0,0,1,176,224C96.6,224,32,159.4,32,80A56.26,56.26,0,0,1,80.92,24.12a16,16,0,0,1,16.62,9.52l21.12,47.15,0,.12A16,16,0,0,1,117.39,96c-.18.27-.37.52-.57.77L96,121.45c7.49,15.22,23.41,31,38.83,38.51l24.34-20.71a8.12,8.12,0,0,1,.75-.56,16,16,0,0,1,15.17-1.4l.13.06,47.11,21.11A16,16,0,0,1,231.88,175.08Z" />
-                            </svg>
-                            07533 904 894
-                        </p>
-                        <p className="text-[#4e8597] text-base font-normal leading-normal flex items-center gap-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 256 256">
                                 <path d="M224,48H32a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8H224a8,8,0,0,0,8-8V56A8,8,0,0,0,224,48ZM98.71,128,40,181.81V74.19Zm11.84,10.85,12,11.05a8,8,0,0,0,10.82,0l12-11.05,58,53.15H52.57ZM157.29,128,216,74.19V181.81Z" />
                             </svg>
                             badiabenmouna@hotmail.com
